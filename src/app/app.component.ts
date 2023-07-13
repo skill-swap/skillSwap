0b705c4c5057d5e8480e8fef8c2a50b9a62c1f56
Oleksandr <61159320+ZIMC-AYZI@users.noreply.github.com>
2023-07-13 17:59:00 +0300

    test sonar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'skill-swap';
+  private kaka = 1;
+
+  public ngOnInit() {
+    const a = 1;
+    const b = 2;
+    let ac = 1;
+    console.log(a);
+    console.log(a);
+    console.log(a);
+    console.log(a);
+    console.log(a);
+    console.log(a);
+
+    [1,2,3].forEach((el) => {
+      console.log(el, 'el');
+    })
+  }
 }
